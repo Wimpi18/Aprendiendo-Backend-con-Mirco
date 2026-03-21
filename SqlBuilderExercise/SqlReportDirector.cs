5323ca5a33e88a01a28c6d0f216853bf75c24a19
Winsor <winsoromar12@gmail.com>
2026-03-20 23:11:10 -0400

    feat: add report director

---
+++ SqlBuilderExercise/SqlReportDirector.cs
@@ -0,0 +1,23 @@
+﻿public class SqlReportDirector
+{
+    public SqlQuery BuildActiveUsersReport()
+    {
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.SetTable("Usuarios");
+        builder.AddColumn("Nombre");
+        builder.AddColumn("Email");
+        builder.AddWhere("Estado = 'Activo'");
+        return builder.Build();
+    }
+
+    public SqlQuery BuildHighValueSalesReport()
+    {
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.SetTable("Ventas");
+        builder.AddColumn("Id");
+        builder.AddColumn("Total");
+        builder.AddColumn("Fecha");
+        builder.AddWhere("Total > 10000");
+        return builder.Build();
+    }
+}
