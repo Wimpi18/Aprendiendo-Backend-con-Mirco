70918b4bb19f4f1cbf86743ef87036d7300bc393
Winsor <winsoromar12@gmail.com>
2026-04-07 23:04:32 -0400

    feat: add injection dependency with adapter

--- SMSAdapter/Program.cs
+++ SMSAdapter/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton<INotifier, CloudSmsAdapter>(provider => new CloudSmsAdapter(
+    new CloudSmsService(),
+    "555-1234"
+));
