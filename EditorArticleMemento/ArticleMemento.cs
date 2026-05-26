dcdd2fbb4c97a50b26aa5479e66aae7de21b9e87
Winsor <winsoromar12@gmail.com>
2026-05-26 00:15:02 -0400

    feat: add ArticleMemento class for article state management

---
+++ EditorArticleMemento/ArticleMemento.cs
@@ -0,0 +1,11 @@
+public class ArticleMemento
+{
+    private readonly string Title;
+    private readonly string Content;
+
+    public ArticleMemento(string title, string content)
+    {
+        Title = title;
+        Content = content;
+    }
+}
